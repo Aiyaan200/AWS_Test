4c8b07626134073cfdcb0d55a0c0026f8ec24865
Aiyaan200 <125077174+Aiyaan200@users.noreply.github.com>
2023-06-08 12:58:44 -0400

    Update AWS_PHP.php

--- AWS_PHP.php
+++ AWS_PHP.php
@@ -3,13 +3,13 @@ session_start();
 $Name = "";
 $Email = "";
 
-$db = mysqli_connect();
+$db = mysqli_connect('database-2.cstj1r9ft2ji.us-east-2.rds.amazonaws.com','admin','Asna200$','Input_Data','3306');
 
 if(isset($_POST['reg_user'])){
-    $FName = mysqli_real_escape_string($db, $_POST['Name']);
+    $Name = mysqli_real_escape_string($db, $_POST['Name']);
     $Email = mysqli_real_escape_string($db, $_POST['Email']);
 
-    $query = "INSERT INTO Test (Name, Email)
+    $query = "INSERT INTO Input_Data (Name, Email)
                 Values('$Name','$Email')";
     mysqli_query($db, $query);
 }
